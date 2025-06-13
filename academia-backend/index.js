c993279d7c482ace4a62fd7d5b501a25fd36183c
dinesh-gurusamy <dineshgurusamy18@gmail.com>
2025-06-13 23:11:58 +0530

    Update index.js

--- academia-backend/index.js
+++ academia-backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000', // ✨ Allow frontend origin
+  origin: 'https://academic-paper-vault.onrender.com', // ✨ Allow frontend origin
   credentials: true,
 }));
 app.use(express.json());
